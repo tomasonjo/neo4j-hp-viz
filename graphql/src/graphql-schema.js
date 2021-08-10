212ca45189a0b998355d2a87b001005cdd18403b
Tomaz Bratanic <bratanic.tomaz@gmail.com>
2021-08-10 11:43:09 +0200

    Add babel to graphql

---
+++ graphql/src/graphql-schema.js
@@ -0,0 +1,37 @@
+export const typeDefs = `
+type Character @exclude(operations: [CREATE, UPDATE, DELETE]){
+    name: String
+    first_seen: Int
+    url: String
+    aliases: String
+    nationality: String
+    species: String
+    gender: String
+    house: House @relationship(type: "BELONGS_TO", direction: OUT)
+    loyalty: [Group] @relationship(type: "LOYAL_TO", direction: OUT)
+    family: [Character] @relationship(type: "FAMILY_MEMBER", direction: OUT)
+    pagerank: Float
+    community: Int
+}
+
+type House @exclude(operations: [CREATE, UPDATE, DELETE]){
+    name: String
+}
+
+type Group @exclude(operations: [CREATE, UPDATE, DELETE]){
+    name: String
+}
+
+type Interaction @exclude(operations: [CREATE, UPDATE, DELETE]){
+    source: Character @relationship(type: "INTERACTS", direction: IN)
+    target: Character @relationship(type: "INTERACTS", direction: OUT)
+    first_seen: Int
+}
+
+type Query{
+    characterSearch(search: String):[Character] @cypher(statement:"""
+        CALL db.index.fulltext.queryNodes('characterSearch', $search + '*') YIELD node
+        RETURN node LIMIT 5
+    """)
+}
+`;
--- graphql/src/index.js
+++ graphql/src/index.js
@@ -1,46 +1,9 @@
-const { Neo4jGraphQL } = require("@neo4j/graphql");
-const neo4j = require("neo4j-driver");
-const { ApolloServer } = require("apollo-server");
+import { Neo4jGraphQL } from "@neo4j/graphql";
+import neo4j from "neo4j-driver";
+import { ApolloServer } from "apollo-server";
+import { typeDefs } from './graphql-schema'
 
 const PORT = 4000
-
-const typeDefs = `
-    type Character @exclude(operations: [CREATE, UPDATE, DELETE]){
-        name: String
-        first_seen: Int
-        url: String
-        aliases: String
-        nationality: String
-        species: String
-        gender: String
-        house: House @relationship(type: "BELONGS_TO", direction: OUT)
-        loyalty: [Group] @relationship(type: "LOYAL_TO", direction: OUT)
-        family: [Character] @relationship(type: "FAMILY_MEMBER", direction: OUT)
-        pagerank: Float
-        community: Int
-    }
-
-    type House @exclude(operations: [CREATE, UPDATE, DELETE]){
-        name: String
-    }
-
-    type Group @exclude(operations: [CREATE, UPDATE, DELETE]){
-        name: String
-    }
-
-    type Interaction @exclude(operations: [CREATE, UPDATE, DELETE]){
-        source: Character @relationship(type: "INTERACTS", direction: IN)
-        target: Character @relationship(type: "INTERACTS", direction: OUT)
-        first_seen: Int
-    }
-
-    type Query{
-        characterSearch(search: String):[Character] @cypher(statement:"""
-            CALL db.index.fulltext.queryNodes('characterSearch', $search + '*') YIELD node
-            RETURN node LIMIT 5
-        """)
-    }
-`;
 
 const driver = neo4j.driver(
     "bolt://neo4j:7687",
